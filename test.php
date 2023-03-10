f1823a80e46157a91d5f8f904d422ab5acb13aee
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-10 15:46:55 +0800

    seatwork

--- test.php
+++ test.php
@@ -8,17 +8,37 @@
 </head>
 <body>
     <?php
-        $count = 1;
-        $arr = array (  "coffeebean" => "coffee", 
+        $count1 = 1;
+        $restaurants = array (  "coffeebean" => "coffee", 
                         "sansrival" => "cakes",
                         "stateresa" => "filipinofood",
                         "2story" => "koreanfood",
                         "mooon" => "mexicanfood");
         echo "Restaurants of Dumaguete and their specialties:<br>";
 
-        foreach($arr as $place => $food){
-            echo $count. ". " . $place . " has " . $food . "<br>" ;
-            $count++;
+        foreach($restaurants as $place => $food){
+            echo $count1. ". " . $place . " has " . $food . "<br>" ;
+            $count1++;
+        }
+
+        echo "<br>";
+
+        $count2 = 1;
+        function display($key, $value){
+            echo $key . "has a value of " . $value . "<br>";
+        }
+
+        foreach($restaurants as $place => $food){
+            display($food, $count2);
+            $count2++;
+        }
+
+        echo "<br>";
+
+        foreach($restaurants as $place => $food){
+            if(strcmp($food, "koreanfood") == 0){
+                echo "The food " . $food . " exists.";
+            }
         }
     ?>
 </body>
