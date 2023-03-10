01a8c3d6de3d15b0ebb9e3f41bc30675722f2f2a
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-10 13:28:34 +0800

    seatwork print individual keys and value

--- test.php
+++ test.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Testing php</title>
+    <title>PHP Seatwork</title>
 </head>
 <body>
-    <p>test</p>
-
     <?php
-        echo "success! testing";
+        $count = 1;
+        $arr = array (  "coffeebean" => "coffee", 
+                        "sansrival" => "cakes",
+                        "stateresa" => "filipinofood",
+                        "2story" => "koreanfood",
+                        "mooon" => "mexicanfood");
+        echo "Restaurants of Dumaguete and their specialties:<br>";
+
+        foreach($arr as $place => $food){
+            echo $count. ". " . $place . " has " . $food . "<br>" ;
+            $count++;
+        }
     ?>
 </body>
 </html>
