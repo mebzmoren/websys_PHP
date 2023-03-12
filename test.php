bb627c05fb03f55ad2ff5d3b1e7a7103f3274bdd
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-12 11:14:47 +0800

    practice

--- test.php
+++ test.php
@@ -8,41 +8,125 @@
 </head>
 <body>
     <?php
-        $count1 = 1;
-        $restaurants = array (  "coffeebean" => "coffee", 
-                        "sansrival" => "cakes",
-                        "stateresa" => "filipinofood",
-                        "2story" => "koreanfood",
-                        "mooon" => "mexicanfood");
-        echo "Restaurants of Dumaguete and their specialties:<br>";
+        //printing
+        echo "Hello!";
+        print("Hello!");        //another way to print
+        echo "Hello" . "World"; //to concatenate strings
 
-        //Create a foreach loop to access each element.
-        foreach($restaurants as $place => $food){
-            echo $count1. ". " . $place . " has " . $food . "<br>" ;
-            $count1++;
+        //varrialbles
+        $var1 = "Hello";        //to declare a variable
+        $var2 = 1234;
+        $var3 = null;
+        $var4 = 1.0;
+        $var5 = 1;
+
+        echo "<br>$var1<br>";   //to print variable
+        $var1;                  //will not print and won't display as an error
+        var_dump($var1);        //will print the structured information
+        var_dump($var2);
+        var_dump($var3);
+        var_dump($var4);
+
+        define("school", "<br>Silliman University");    //constant, can't be modified
+        define("score", 100, true);
+        echo school;
+        echo score;
+
+        if($var4 == $var5)
+            echo "<br>Equal";
+        else
+            echo "<br>Not Equal";
+
+        if($var4 === $var5)
+            echo "<br>Same Type";
+        else
+            echo "<br>Different Type";
+
+        echo "<br>";
+
+        //switch
+        switch($var1){
+            case "Hello":
+                echo $var1;
+                break;
+            case 1234:
+                echo $var2;
+                break;
+            case null:
+                echo $var3;
+                break;
+            case 1.0;
+                echo $var4;
+                break;
+            case 1;
+                echo $var5;
+                break;
+            default:
+                echo "none";
+                break;
         }
 
         echo "<br>";
 
-        //Pass every element to a function and display the value there.
-        $count2 = 1;
-        function display($key, $value){
-            echo $key . "has a value of " . $value . "<br>";
+        //loops
+        while($var5 < 3){
+            echo $var5 . "<br>";
+            $var5++;
         }
 
-        foreach($restaurants as $place => $food){
-            display($food, $count2);
-            $count2++;
+        do{
+            echo $var5 . "<br>";
+            $var5++;
+        }while($var5 < 8);
+
+        for($var5; $var5 < 12; $var5++){
+            echo $var5 . "<br>";
         }
+
+        //function
+        function thisone($var6){
+            if ($var6 == 10)
+                echo "Equal";
+            else
+                echo "Not Equal";
+        }
+
+        thisone(5);
 
         echo "<br>";
 
-        //Search for the string “koreanfood” using strcmp function. Prompt a message if a match is found.
-        foreach($restaurants as $place => $food){
-            if(strcmp($food, "koreanfood") == 0){
-                echo "The food " . $food . " exists.";
-            }
+        //returning values
+        function f1($var7=20){
+            return ++$var7;
         }
+
+        echo f1();
+
+        $var8 = f1(3);
+        echo "<br>" . $var8;
+
+        f1(9);
+
+        echo "<br>";
+
+        //arrays
+        $fruits = array("apple", "mango", "orange");
+
+        echo count($fruits);
+        $length = count($fruits);
+
+        for($x = 0; $x < $length; $x++){
+            echo "<br>" . $fruits[$x];
+        }
+
+        $age = array(   "Peter" => "35",
+                        "Ben" => "37");
+        
+        foreach($age as $key){
+            echo "<br> $key";
+        }
+
+        phpinfo();  //outputs how the version of php was configured
     ?>
 </body>
 </html>
